1ed57cc9ec1df150cfea4dbff4699124fc73edf0
QuadX <quadx.vlad@gmail.com>
2018-03-08 15:58:11 +0300

    Added Config class for setting NumpyEncoder as RESTFUL_JSON cls encoder

--- api.py
+++ api.py
@@ -2,18 +2,26 @@ from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from werkzeug.datastructures import FileStorage
 
+from face import FaceExtractor
 from image_preprocessing import load_image
 from model.inception_resnet_v1 import InceptionResNetV1
-from settings import MODEL_WEIGHTS_PATH, DEBUG
+from settings import DEBUG, MODEL_WEIGHTS_PATH
+from utils import NumpyEncoder
 
 app = Flask(__name__)
-app.debug = DEBUG
 api = Api(app)
+app.debug = DEBUG
 model = None
+face_extractor = None
+
+
+class Config:
+    RESTFUL_JSON = {'cls': NumpyEncoder}
 
 
 class RecognizeFace(Resource):
     def post(self):
+        global model
         parse = reqparse.RequestParser()
         parse.add_argument('image', type=FileStorage, location='files')
         args = parse.parse_args()
@@ -21,13 +29,16 @@ class RecognizeFace(Resource):
         image = load_image(_image, 160)
         return {
             'image': _image.filename,
-            'embeddings': model.predict(image)[0].tolist()
+            'embeddings': model.predict(image)[0]
         }
 
 
 api.add_resource(RecognizeFace, '/recognize-face')
 
 if __name__ == '__main__':
+    app.config.from_object(Config)
+
+    face_extractor = FaceExtractor()
     print('Loading Face Recognition model...')
     model = InceptionResNetV1()
     model.load_weights(MODEL_WEIGHTS_PATH)
