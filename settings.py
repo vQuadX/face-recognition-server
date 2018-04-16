cf900034f4e459073d8a5874170982f0c7aff470
QuadX <quadx.vlad@gmail.com>
2018-04-16 15:07:26 +0300

    Models moved to models folder

--- settings.py
+++ settings.py
@@ -5,5 +5,5 @@ import cv2
 DEBUG = False
 
 BASE_PATH = os.path.dirname(__file__)
-MODEL_WEIGHTS_PATH = os.path.join(BASE_PATH, 'model/keras_facenet_weights.h5')
+MODEL_WEIGHTS_PATH = os.path.join(BASE_PATH, 'models/keras_facenet_weights.h5')
 HAARCASCADE_PATH = os.path.join(cv2.data.haarcascades, 'haarcascade_frontalface_default.xml')
