1955267cdcc5ed3ae69ee8b55ba21170a6bffed4
QuadX <quadx.vlad@gmail.com>
2018-05-28 14:17:46 +0300

    Added FACE_RECOGNITION_THRESHOLD parameter

--- settings.py
+++ settings.py
@@ -8,9 +8,10 @@ DEBUG = False
 
 BASE_PATH = os.path.dirname(__file__)
 MODEL_WEIGHTS_PATH = os.path.join(BASE_PATH, 'models/keras_facenet_weights.h5')
-CLASSIFIER_PATH = os.path.join(BASE_PATH, 'models/knn_classifier2.pkl')
+CLASSIFIER_PATH = os.path.join(BASE_PATH, 'models/knn_classifier_new.pkl')
 HAARCASCADE_PATH = os.path.join(cv2.data.haarcascades, 'haarcascade_frontalface_default.xml')
+
+FACE_RECOGNITION_THRESHOLD = 0.6
 
 RESTFUL_JSON = {'cls': NumpyEncoder}
 JWT_SECRET_KEY = 'dQeyjj5YvqCRdP6C8EH47FTmKDg6yPE0'
-JWT_VERIFY_EXPIRATION = False
